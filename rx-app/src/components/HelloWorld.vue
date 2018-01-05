6f614d70d50c3ce45cee62cb349ed13aab7ad977
pengjiecheng <pcj212@126.com>
2018-01-05 18:05:14 +0800

    start

---
+++ rx-app/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+   
+  </div>
+</template>
+
+<script>
+export default {
+ 
+  data () {
+    return {
+      msg: 'Welcome to rx App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
