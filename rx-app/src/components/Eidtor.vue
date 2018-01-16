0caf24975726a6e2ca1ec933dd8a32b5d3d82c5f
pengjiecheng <pcj212@126.com>
2018-01-16 19:19:03 +0800

    准备学习

--- rx-app/src/components/Eidtor.vue
+++ rx-app/src/components/Eidtor.vue
@@ -1,13 +1,42 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="note-editor">
+    <div class="form-group">
+      <input type="text" 
+        name="title" 
+        class="title form-control"
+        placeholder="请输入标题"
+        v-model="currenNote.title">
+      <textarea name="content" class="form-control"  rows=""
+        placeholder="请输入正文"
+        @input="updateNote"></textarea>
+    </div>
     
   </div>
 </template>
 
 <script>
+import { mapActions,mapState,mapGetters } from 'vuex';
 export default {
- 
+  name:'Editor',
+  computed:{
+    ...mapGetters([
+      'activeNote'
+    ]),
+    currenNote(){
+      return this.activeNote;
+    }
+  },
+  methods:{
+    ...mapActions({
+      update:'updateNote'
+    }),
+    updateNote(){
+      this.update({
+        note:this.currenNote
+      })
+    }
+  },
+
   data () {
     return {
       msg: '笔记内容'
@@ -18,5 +47,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+ .note-editor {
+    flex:2;
+  }
+  .note-editor textarea {
+    box-sizing: border-box;
+    height: 100%;
+    border: 0;
+    border-radius: 0;
+    padding-top: 15px;
+  }
+  .form-group {
+    display: flex;
+    flex-flow: column;
+    height: 100%;
+    margin-bottom: 0;
+  }
+  .form-group textarea {
+    flex-grow: 2;
+  }
+  .title {
+    border:0;
+  }
 </style>
--- rx-app/src/components/NotesList.vue
+++ rx-app/src/components/NotesList.vue
@@ -1,22 +1,142 @@
 <template>
-  <div >
-    <h1>{{ msg }}</h1>
+  <div class="notes-list">
+    <div class="list-header">
+      <h2>Notes</h2>
+      <div class="btn-group btn-group-justified" role="group">
+          <!--all  -->
+          <div class="btn-group" role="group">
+            <button type="button" class="btn btn-default"
+            @click="toggleShow('all')"
+            :class="{active:show === 'all'}"
+            >
+            全部笔记
+            </button>
+          </div>
+
+          <!-- favorites -->
+          <div class="btn-group" role="group">
+            <button type="button" class="btn btn-default"
+            @click="toggleShow('favorite')"
+            :class="{active:show ==='favorote'}"
+            >
+              收藏夹</button>
+          </div>
+
+          <!-- search -->
+          <div class="btn-group btn-group-justified" role="group">
+            <div class="input-group search">
+              <input type="text" class="form-control" v-model="search"
+              placeholder="输入搜索内容">
+              <span class="input-group-addon">
+                <i class="glyphicon glyphicon-search"></i>
+              </span>
+            </div>
+          </div>
+      </div>
+
+    <!-- render list-->
+    <div class="container">
+      <div class="list-group">
+        <div
+        v-for="(note,index) in searchNotes" :key=index
+        class="list-group-item"
+        :class="{active:activeNote === note}"
+        @click="updateActiveNote(note)">
+        <h4 class="list-group-item-heading">
+          {{note.title.trim().substring(0,30)}}
+        </h4>
+        </div>
+      </div>
+    </div>
+    </div>
+    
    
   </div>
 </template>
 
 <script>
+import { mapGetters,mapState,mapActions} from 'vuex';
+
 export default {
- 
-  data () {
+  name:'NoteList',
+  data(){
     return {
-      msg: '笔记列表'
+      search:''
+    }
+  },
+  computed:{
+    ...mapGetters([
+      'filteredNotes'
+    ]),
+    //stae 内部状态
+    ...mapState([
+      'show',
+      'activeNote'
+    ]),
+    //计算属性，定义过滤
+    searchNotes(){
+      if(this.search.length>0){
+        return this.filteredNotes.filter((note) => note.title.toLowerCase().indexOf(this.search)>-1)
+      }else{
+        return this.filteredNotes;
+      }
+    }
+  },
+  methods:{
+    ...mapActions([
+      'toggleListShow',
+      'setActiveNote'
+    ]),
+    //切换列表，全部或者收藏
+    toggleShow(type){
+      this.toggleListShow({show:type});
+    },
+    //点击列表,更新当前激活文章
+    updateActiveNote(note){
+      this.setActiveNote({note})
     }
   }
+ 
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.notes-list {
+    flex:1;
+    background-color: #F5F5F5;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+  }
+  .list-header {
+    padding: 5px 25px 25px 25px;
+  }
+  .list-header .search{
+    margin-top: 20px;
+  }
+  .list-header h2 {
+    font-weight: 300;
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 22px;
+    padding-bottom: 8px;
+  }
+  .container {
+    height: calc(100% - 204px);
+    max-height: calc(100% - 204px);
+    overflow: auto;
+    width: 100%;
+    padding: 0;
+  }
+  .container .list-group-item{
+    border: 0;
+    border-radius: 0;
+  }
+  .list-group-item-heading{
+    font-weight: 300;
+    font-size: 15px;
+  }
+  .btn-group, .btn-group-vertical{
+    vertical-align:bottom;
+  }
 </style>
